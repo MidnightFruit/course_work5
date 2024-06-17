398a7b4375755af2027fd872e438a1d91882b67f
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-06-17 22:50:16 +0700

    Добавлена логика работы с БД

--- DBManage/utils.py
+++ DBManage/utils.py
@@ -1,12 +1,12 @@
 from typing import Any
-
+from configparser import ConfigParser
 import requests
 
 from APImanger.HeadHunterAPI import HeadHunterAPI
 
 
 def format_companies(data: dict):
-    result = [{}]
+    result = []
     for company_name, company_key in data.items():
         url = f"https://api.hh.ru/employers/{company_key}"
         company_info = requests.get(url).json()
@@ -17,10 +17,24 @@ def format_companies(data: dict):
 
 
 def format_vacancy(data: list[dict[str, Any]]):
-    result = [{}]
+    result = []
     for vacancy in data:
         temp_dict = {"vacancy_id": vacancy['id'], "company_id": vacancy['employer']['id'],
                      "salary": vacancy['salary'], "vacancy_url": vacancy['url'],
                      "description": vacancy['snippet']['requirement']}
         result.append(temp_dict)
     return result
+
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception()
+
+    return db
